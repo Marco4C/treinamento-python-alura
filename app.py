1eec89b3568556bd90b0f3388e72db24b0ff339d
Marco4C <marco4coutinho@gmail.com>
2024-05-14 23:58:40 -0300

    aulas 4 e 5 (final)

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
+from ast import List
+from ctypes import Array
 import os
 import csv
 
 restaurantes = [
+    {"nome": "nome", "categoria": "categoria", "ativo": "ativo"},
     {"nome": "Praça", "categoria": "Japonesa", "ativo": "Inativo"},
+    {"nome": "Pizza Superma", "categoria": "Pizza", "ativo": "Ativo"},
+    {"nome": "Pizza Superma", "categoria": "Pizza", "ativo": "Ativo"},
+    {"nome": "Pizza Superma", "categoria": "Pizza", "ativo": "Ativo"},
+    {"nome": "Pizza Superma", "categoria": "Pizza", "ativo": "Ativo"},
+    {"nome": "Pizza Superma", "categoria": "Pizza", "ativo": "Ativo"},
     {"nome": "Pizza Superma", "categoria": "Pizza", "ativo": "Ativo"},
     {"nome": "Cantina", "categoria": "Italiano", "ativo": "Inativo"},
 ]
@@ -34,12 +42,13 @@ def finalizar_app():
         os.system("cls")
         print("This is a Windows system.")
         print(os.name)
+        print("Finalizando o app")
     if os.name == "posix":  # posix para linux ou mac os
         os.system("clear")
         print("This is a Linux or MacOS system.")
         print(os.name)
+        print("Finalizando o app")
     # os.system('clear')
-    print("Finalizando o app")
 
 
 def opcao_invalida():
@@ -53,19 +62,16 @@ def voltar_ao_menu_principal():
 
 
 def escolher_opcao():
-    try:
-        opcao_escolhida = int(input("Escolha uma opção: "))
-        if opcao_escolhida == 1:
-            cadastrar_novo_restaurante()
-        elif opcao_escolhida == 2:
-            listar_restaurantes()
-        elif opcao_escolhida == 3:
-            print("Ativar restaurante")
-        elif opcao_escolhida == 4:
-            finalizar_app()
-        else:
-            opcao_invalida()
-    except:
+    opcao_escolhida = int(input("Escolha uma opção: "))
+    if opcao_escolhida == 1:
+        cadastrar_novo_restaurante()
+    elif opcao_escolhida == 2:
+        listar_restaurantes()
+    elif opcao_escolhida == 3:
+        print("Ativar restaurante")
+    elif opcao_escolhida == 4:
+        finalizar_app()
+    else:
         opcao_invalida()
 
 
@@ -79,7 +85,7 @@ def cadastrar_novo_restaurante():
 
 
 def exibir_subtitulo(texto):
-    os.system("cls")
+    os.system("clear")
     print(texto)
     print()
 
@@ -97,26 +103,36 @@ def salva_restaurantes():
             escritor.writerow(restaurante)
 
 
-def escreve_restaurantes():
+def consultar_restaurantes() -> list:
+    lista_restaurantes = []
     with open("restaurantes.csv") as restaurantes:
         leitor = csv.DictReader(restaurantes)
         for linha in leitor:
-            print(linha)
+            lista_restaurantes.append(linha)
+    return lista_restaurantes
 
 
 def listar_restaurantes():
     exibir_subtitulo("Listando restaurantes")
+    lista_restaurantes = consultar_restaurantes()
+    restaurantes = lista_restaurantes
 
+    print(f"restaurantes: {restaurantes}")
     for restaurante in restaurantes:
-        print(f".{restaurante}")
+        print(f"restaurante: {restaurante}")
 
-    escreve_restaurantes()
+    print(f"{'Nome do restaurante'.ljust(22)} | {'Categoria'.ljust(20)} | Status")
+    for restaurante in restaurantes:
+        nome_restaurante = restaurante["nome"]
+        categoria = restaurante["categoria"]
+        ativo = "ativado" if restaurante["ativo"] else "desativado"
+        print(f"- {nome_restaurante.ljust(20)} | {categoria.ljust(20)} | {ativo}")
+
     voltar_ao_menu_principal()
 
 
 def main():
     os.system("clear")
-    salva_restaurantes()
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
